bf623ea45007aaf3213bb4f34feb661b5c463b50
20120551 <killermh1352@gmail.com>
2022-12-15 18:16:30 +0700

    fix bug

--- src/Router/goods-router.js
+++ src/Router/goods-router.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const {GoodsController} = require('./../Controller');
+const { GoodsController } = require('./../Controller');
 const { authorizationMDW } = require('./../Middleware');
 const router = express.Router();
 const goodsController = new GoodsController();
 
-router.get('/storeroom',authorizationMDW.checkUser, goodsController.getStoreRoom);
+router.get('/storeroom', goodsController.getStoreRoom);
 router.post('/addGood', authorizationMDW.checkUser, goodsController.addGood);
-router.get('/:id',authorizationMDW.checkUser, goodsController.getGoodByID);
-router.post('/:id',authorizationMDW.checkUser, goodsController.updateGoodByID);
-router.delete('/:id',authorizationMDW.checkUser, goodsController.deleteGoodByID);
-router.get('/type/:dishType',authorizationMDW.checkUser, goodsController.getGoodsByType);
+router.get('/:id', goodsController.getGoodByID);
+router.post('/:id', authorizationMDW.checkUser, goodsController.updateGoodByID);
+router.delete('/:id', authorizationMDW.checkUser, goodsController.deleteGoodByID);
+router.get('/type/:dishType', goodsController.getGoodsByType);
 
 module.exports = router;
